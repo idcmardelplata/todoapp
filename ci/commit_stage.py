8bde4a115135781023aa179f90c41217bc591308
Martin Algañaraz <idcmardelplata@gmail.com>
2023-08-27 18:27:47 -0300

    ci: refactor dagger pipeline

--- ci/commit_stage.py
+++ ci/commit_stage.py
@@ -1,9 +1,10 @@
 import sys
+from typing import Dict
 import dagger
 import anyio
 
+async def run(command: str):
 
-async def main():
     config = dagger.Config(log_output=sys.stdout)
     async with dagger.Connection(config) as client:
         cache = client.cache_volume("pip")
@@ -12,25 +13,25 @@ async def main():
             .from_("idcmardelplata/poetry:v02")
             .with_directory("/src", client.host().directory("."), exclude=["ci/"])
             .with_workdir("/src")
-            # .with_exec(["poetry", "config", "cache-dir", "/src/.cache/"])
             .with_env_variable("POETRY_CACHE_DIR", "/src/.cache")
             .with_mounted_cache("/src/.cache", cache)
             .with_exec(["poetry", "install"])
             .with_mounted_cache("/src/.cache", cache)
-
         )
 
-        linter = (
-                python
-                .with_exec(["poetry", "run", "black", "."])
-                )
-        unittest = (
-                python
-                .with_exec(["poetry", "run", "pytest"])
-                )
+        linter = ( python.with_exec(["poetry", "run", "black", "."]))
+        unittest = ( python.with_exec(["poetry", "run", "pytest"]))
 
-        await linter.stdout()
-        await unittest.stderr()
+        options = {'linter': linter.stdout, 'unittest': unittest.stderr}
 
+        if command == 'both':
+            await linter.stdout()
+            await unittest.stderr()
+        else:
+            await options.get(command)()
+
+
+async def main():
+    await run("both")
 
 anyio.run(main)
